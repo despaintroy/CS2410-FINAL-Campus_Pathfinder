80f9c8e7c9cd2814b2650fce362451bfbd17efd9
despaintroy <despaintroy@gmail.com>
2020-03-03 22:20:20 -0700

    Pathfinding is better, but still doesn't produce the best path

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Dijkstra {
 
     double [][] graph;
-    ArrayList<Integer> traversed;
+    PathNode[] pathNodes;
     int start;
     int destination;
 
@@ -11,85 +11,106 @@ public class Dijkstra {
         this.graph = graph;
         this.start = start;
         this.destination = destination;
-        traversed = new ArrayList<>();
+        pathNodes = new PathNode[graph.length];
+
+        for (int i=0; i<pathNodes.length; i++) {
+            pathNodes[i] = new PathNode();
+        }
     }
 
     public ArrayList<Integer> findPath() {
 
         ArrayList<Integer> best = new ArrayList<Integer>();
 
-        Node N = new Node(0, null);
+        pathNodes[start].distance = 0;
+        pathNodes[start].parent = start;
 
-        Node found = find(N);
+        find(start);
 
-        while (found != null) {
-            best.add(0, found.value);
-            found = found.parent;
+        int curr = destination;
+
+        while (curr != start) {
+            best.add(0, curr);
+            curr = pathNodes[curr].parent;
         }
+        best.add(0, pathNodes[curr].parent);
 
         return best;
     }
 
-    private Node find(Node curr) {
-
-        traversed.add(curr.value);
+    private void find(int curr) {
 
 //        System.out.println(
-//                "\n\nCurrent: " + curr.value +
-//                "\nParent: " + curr.parent
+//                "\n\nCurrent: " + curr +
+//                "\nParent: " + pathNodes[curr].parent
 //        );
 
-        if (curr.value == destination) {
+        // Detect if the destination node is found
+        if (curr == destination) {
 //            System.out.println("My value is the destination!");
-            return curr;
+            return;
         }
 
-        // Find all nodes connected to this one
-        ArrayList<Node> nextNodes = new ArrayList<>();
+        // Find all nodes connected to this one that we can travel to
+        ArrayList<Integer> nextList = new ArrayList<>();
 
         for (int i=0; i<graph.length; i++) {
-            for (int j=0; j<graph[i].length; j++) {
-                int temp;
-                if (graph[i][j]!=-1 && i==curr.value && j!=i) {
-                    nextNodes.add(new Node(j, curr));
-                }
-                if (graph[i][j]!=-1 && j==curr.value && i!=j) {
-                    nextNodes.add(new Node(i, curr));
-                }
-            }
-        }
+            for (int j=0; j<i; j++) {
 
-        // Eliminate all nodes already traversed
-        for (int i=0; i<nextNodes.size(); i++) {
-            if (traversed.contains(nextNodes.get(i).value)) {
-                nextNodes.remove(i);
-                i--;
+                // Determine if this edge is on the graph
+                if (graph[i][j] >= 0) {
+
+                    int next = -1;
+
+                    // Find if this is a next node
+                    if (i == curr) {
+                        next = j;
+                    } else if (j == curr) {
+                        next = i;
+                    }
+
+                    if (next>=0) {
+
+                        // If this next node has a longer path, update it's parent
+                        if (pathNodes[next].distance > pathNodes[curr].distance + graph[Math.max(curr,next)][Math.min(curr,next)]) {
+
+                            // If it didn't have a parent, we'll add it to the next list
+                            if (pathNodes[next].parent < 0) {
+                                nextList.add(next);
+//                                pathNodes[next].parent = curr;
+
+//                            System.out.println("Added to next: " + j);
+                            }
+
+                            pathNodes[next].distance = pathNodes[curr].distance + graph[Math.max(curr,next)][Math.min(curr,next)];
+                            pathNodes[next].parent = curr;
+                        }
+                    }
+                }
             }
         }
 
         // For each node, traverse
-        for (Node n : nextNodes) {
-//            System.out.println("Next search: " + n.value);
-            Node found = find(n);
-            if (found != null) {
-                return found;
-            }
+        for (int n : nextList) {
+//            System.out.println("Next search: " + n);
+            find(n);
         }
 
-        return null;
+        return;
     }
 
-    private class Node {
-        int value;
-        Node parent;
+    private class PathNode {
+        int parent;
+        double distance;
 
-        public Node(int value, Node parent) {
-            this.value = value;
+        public PathNode() {
+            parent = -1;
+            distance = Double.MAX_VALUE;
+        }
+
+        public PathNode(int parent, double distance) {
             this.parent = parent;
+            this.distance = distance;
         }
-    }
-
-    private boolean alreadyTraversed(int i) {
-        return traversed.contains(i);
     }
 }
--- src/MapView.java
+++ src/MapView.java
@@ -15,6 +15,7 @@ public class MapView {
     final int VIEWPORT_WIDTH;
     final int VIEWPORT_HEIGHT;
     Pane viewPane;
+    Pane graph;
     Graph newGraph;
 
     public MapView(int viewport_width, int viewport_height) {
@@ -27,7 +28,7 @@ public class MapView {
 //        System.out.println(newGraph);
 
         StackPane stack = new StackPane();
-        Pane graph = new Pane();
+        graph = new Pane();
 
         try {
             FileInputStream inputstream = new FileInputStream("campus_map.png");
@@ -71,11 +72,33 @@ public class MapView {
 
     public void findPath() {
 
-        Dijkstra pathFinder = new Dijkstra(newGraph.adjacency, 0, 1);
+        int start = 100;
+        int end = 500;
+
+        Dijkstra pathFinder = new Dijkstra(newGraph.adjacency, start, end);
         ArrayList<Integer> bestPath = pathFinder.findPath();
 
-        System.out.println(bestPath);
+        // Mark with circles the endpoints
+        Circle startCircle = new Circle(newGraph.nodes.get(start).x/SCALE, newGraph.nodes.get(start).y/SCALE, 5 , Style.edgeColor);
+        Circle endCircle = new Circle(newGraph.nodes.get(end).x/SCALE, newGraph.nodes.get(end).y/SCALE, 5 , Style.edgeColor);
 
-        System.out.println("Found a path!");
+        System.out.println("The best path is: " + bestPath);
+
+        graph.getChildren().clear();
+
+        graph.getChildren().addAll(startCircle, endCircle);
+
+        for (int i=0; i<bestPath.size()-1; i++) {
+
+            // Draw the new path
+            float x1 = newGraph.nodes.get(bestPath.get(i)).x;
+            float y1 = newGraph.nodes.get(bestPath.get(i)).y;
+            float x2 = newGraph.nodes.get(bestPath.get(i+1)).x;
+            float y2 = newGraph.nodes.get(bestPath.get(i+1)).y;
+
+            Line temp = new Line(x1/SCALE, y1/SCALE, x2/SCALE, y2/SCALE);
+            temp.setStroke(Style.edgeColor);
+            graph.getChildren().add(temp);
+        }
     }
 }
