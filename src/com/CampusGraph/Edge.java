84eae475901e7807c540cc9ee0e178166f461a1a
despaintroy <despaintroy@gmail.com>
2020-03-17 11:34:37 -0600

    Added support for indoor property in edges class

--- src/com/CampusGraph/Edge.java
+++ src/com/CampusGraph/Edge.java
@@ -4,17 +4,28 @@ class Edge {
 
     private double length;
     private boolean active;
+    private boolean indoors;
 
+    // TODO: add indoors to constructor
     Edge(boolean active, double length) {
         this.active = active;
         this.length = length;
+        this.indoors = false;
     }
 
     double getLength() {
         return length;
     }
 
+    boolean isIndoors() {
+        return this.indoors;
+    }
+
     boolean isActive() {
         return active;
+    }
+
+    void setIndoors(Boolean indoors) {
+        this.indoors = indoors;
     }
 }
