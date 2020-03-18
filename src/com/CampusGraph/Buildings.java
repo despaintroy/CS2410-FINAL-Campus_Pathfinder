11d1f82f27032b99ea6713650581c25141a8a890
despaintroy <despaintroy@gmail.com>
2020-03-18 11:56:44 -0600

    Ability to get building codes

--- src/com/CampusGraph/Buildings.java
+++ src/com/CampusGraph/Buildings.java
@@ -10,14 +10,26 @@ public class Buildings {
 
     public Buildings() {
         // TODO: Get rid of these and have them read in from file instead
-        buildingNodes.put("Life Sciences", 438);
-        buildingNodes.put("Old Main", 37);
-        buildingNodes.put("Engineering", 345);
-        buildingNodes.put("Family Life", 12);
-        buildingNodes.put("Institute", 833);
+        buildingNodes.put("LSB", 438);
+        buildingNodes.put("MAIN", 37);
+        buildingNodes.put("ENGR", 345);
+        buildingNodes.put("FL", 12);
+        buildingNodes.put("LDSI", 833);
     }
 
-    public static int getBuildNodeId(String buildingName) {
-        return buildingNodes.get(buildingName);
+    public static String[] getCodes() {
+
+        String[] buildList = new String[buildingNodes.size()];
+
+        int i=0;
+        for (String building : buildingNodes.keySet()){
+            buildList[i] = building;
+            i++;
+        }
+        return buildList;
+    }
+
+    public static int getBuildNodeId(String buidingCode) {
+        return buildingNodes.get(buidingCode);
     }
 }
